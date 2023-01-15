359ea0f9b8d077b5cdda609b377cf6bb3d3dc4ad
Nurbek Ismoilov <nuronbeck@gmail.com>
2023-01-16 04:02:41 +0500

    Test

--- netlify/functions/scheduled-job.js
+++ netlify/functions/scheduled-job.js
@@ -6,11 +6,29 @@ import { schedule } from "@netlify/functions"
 // Every minute
 // * * * * *
 
+const getTashkentTime = () => {
+  const date = new Date();
+  const dateFormat = new Intl.DateTimeFormat("en-US", {
+    timeZone: "Asia/Tashkent",
+      hour12: false,
+      hour: 'numeric', minute: 'numeric'
+  });
+
+  return dateFormat.format(date);
+}
+
 exports.handler = schedule('* * * * *', async (event) => {
+  const tashkentTime = getTashkentTime();
+
   console.log("Scheduled job running...");
   console.log("-------------");
-  console.log(new Date());
+  console.log(tashkentTime);
   console.log("-------------");
+
+  // Check every minute that matches 10:00
+  if(tashkentTime === '04:05'){
+    console.log(`Matched => ${tashkentTime}`);
+  }
 
   return {
     statusCode: 200,
